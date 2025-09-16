KhronosGroup/antora-ui-khronos

hide expand/collapse button on nav if there are no expandable items

src/js/01-nav.js
@@ -24,7 +24,18 @@
     menuPanel.scrollTop = 0
   }
 
-  if (navMenuToggle) {
+  find(menuPanel, '.nav-item-toggle').forEach(function (btn) {
+    var li = btn.parentElement
+    btn.addEventListener('click', toggleActive.bind(li))
+    var navItemSpan = findNextElement(btn, '.nav-text')
+    if (navItemSpan) {
+      navItemSpan.style.cursor = 'pointer'
+      navItemSpan.addEventListener('click', toggleActive.bind(li))
+    }
+  })
+
+  if (navMenuToggle && menuPanel.querySelector('.nav-item-toggle')) {
+    navMenuToggle.style.display = ''
     navMenuToggle.addEventListener('click', function () {
       var collapse = !this.classList.toggle('is-active')
       find(menuPanel, '.nav-item > .nav-item-toggle').forEach(function (btn) {
@@ -38,16 +49,6 @@
       }
     })
   }
-
-  find(menuPanel, '.nav-item-toggle').forEach(function (btn) {
-    var li = btn.parentElement
-    btn.addEventListener('click', toggleActive.bind(li))
-    var navItemSpan = findNextElement(btn, '.nav-text')
-    if (navItemSpan) {
-      navItemSpan.style.cursor = 'pointer'
-      navItemSpan.addEventListener('click', toggleActive.bind(li))
-    }
-  })
 
   if (explorePanel) {
     explorePanel.querySelector('.context').addEventListener('click', function () {

timothysparg/antora-tldr-ui

src/js/01-nav.js
@@ -39,7 +39,17 @@
 
   fitNavInit({})
 
-  if (navMenuToggle) {
+  find(menuPanel, '.nav-item-toggle').forEach(function (btn) {
+    btn.addEventListener('click', toggleActive.bind(btn.parentElement))
+    var nextElement = btn.nextElementSibling
+    if (nextElement && nextElement.classList.contains('nav-text')) {
+      nextElement.style.cursor = 'pointer'
+      nextElement.addEventListener('click', toggleActive.bind(btn.parentElement))
+    }
+  })
+
+  if (navMenuToggle && menuPanel.querySelector('.nav-item-toggle')) {
+    navMenuToggle.style.display = ''
     navMenuToggle.addEventListener('click', function () {
       var collapse = !this.classList.toggle('is-active')
       find(menuPanel, '.nav-item > .nav-item-toggle').forEach(function (btn) {
@@ -53,15 +63,6 @@
       }
     })
   }
-
-  find(menuPanel, '.nav-item-toggle').forEach(function (btn) {
-    btn.addEventListener('click', toggleActive.bind(btn.parentElement))
-    var nextElement = btn.nextElementSibling
-    if (nextElement && nextElement.classList.contains('nav-text')) {
-      nextElement.style.cursor = 'pointer'
-      nextElement.addEventListener('click', toggleActive.bind(btn.parentElement))
-    }
-  })
 
   nav.querySelector('[data-panel=explore] .context').addEventListener('click', function () {
     find(nav, '[data-panel]').forEach(function (panel) {